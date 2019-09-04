e8084bb8827d6936520dd1a7f0a70df77659e46f
Paul Høglend <pmhoglend@gmail.com>
2019-09-04 14:23:00 +0200

    I oppgave 10: Splittet inputene i bokstavarrays

--- src/Assignments/Oblig1/Oblig1.java
+++ src/Assignments/Oblig1/Oblig1.java
@@ -2,8 +2,13 @@ package Assignments.Oblig1;
 
 public class Oblig1 {
 
+    //TODO: Må fjernes!!!!!!!!
+    // For å sjekke ut ting underveis
+    public static void main(String args[]) {
+        Oblig1.inneholdt("abca","dddd");
+    }
+
     //TODO: implementer metodene
-    //TODO: Bli enige om metodenavn
     public static int maks(int [] values ) {
         return 0;
     }
@@ -41,6 +46,17 @@ public class Oblig1 {
     }
 
     public static boolean inneholdt(String s1, String s2) {
+        String str1 = s1.toUpperCase();
+        String str2 = s2.toUpperCase();
+        String [] strArray1 = str1.split("\\s");
+        String [] strArray2 = str2.split("\\s");
+        //test
+        for(String s: strArray1) {
+            System.out.println(s);
+        }
+        if(s2.length() > s1.length()) {
+            //TODO:Lage exception
+        }
         return false;
     }
 

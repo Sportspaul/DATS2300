abe67d317a1585532929a69d823b36e71a114ea4
Paul Høglend <pmhoglend@gmail.com>
2019-09-04 09:55:41 +0200

    Laget tomme metoder med engelske navn

--- src/Assignments/Oblig1/Oblig1.java
+++ src/Assignments/Oblig1/Oblig1.java
@@ -2,8 +2,51 @@ package Assignments.Oblig1;
 
 public class Oblig1 {
 
-    //TODO: implementer metoden
-    public static int max(int [] values) {
+    //TODO: implementer metodene
+    //TODO: Bli enige om metodenavn
+    public static int max(int [] values ) {
         return 0;
     }
+
+    public static int interchanges(int [] values ) {
+        return 0;
+    }
+
+    public static int numberOfDifferentSorted( int [] values ) {
+        return 0;
+    }
+
+    public static int numberOfDifferentUnsorted( int [] values ) {
+        return 0;
+    }
+
+    public static void partSorted(int [] values) {
+
+    }
+
+    public static void rotation(char [] values, int k) {
+
+    }
+
+    public static String merge(String s1, String s2) {
+        return "";
+    }
+
+    public static String merge(String [] strings) {
+        return "";
+    }
+
+    public static int [] indexSort(int [] values) {
+        return null;
+    }
+
+    public static int [] thirdSmallest(int [] values)  {
+        return null;
+    }
+
+    public static boolean contained(String s1, String s2) {
+        return false;
+    }
+
+
 }

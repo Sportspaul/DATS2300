e92f02e893f8582635571e7d6ba48925c7704099
hvrus <hvrustad@gmail.com>
2019-09-04 12:59:23 +0200

    Forandret til @Test over alle testene, istede for @org.junit.jupiter.api.test på unit testene

--- src/Assignments/Oblig1Test/Oblig1UnitTest.java
+++ src/Assignments/Oblig1Test/Oblig1UnitTest.java
@@ -1,5 +1,7 @@
 package Assignments.Oblig1Test;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -8,57 +10,57 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class Oblig1UnitTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void maks() {
         assertEquals(true, false, "Implementer maks og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void ombyttinger() {
         assertEquals(true, false, "Implementer ombyttinger og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void antallUlikeSortert() {
         assertEquals(true, false, "Implementer antallUlikeSortert og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void antallUlikeUsortert() {
         assertEquals(true, false, "Implementer antallUlikeUsortert og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void delsortering() {
         assertEquals(true, false, "Implementer delsortering og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void rotasjon() {
         assertEquals(true, false, "Implementer rotasjon og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void flett() {
         assertEquals(true, false, "Implementer flett og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void indekssortering() {
         assertEquals(true, false, "Implementer indekssortering og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void tredjeMin() {
         assertEquals(true, false, "Implementer tredjeMin og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void bokstavNr() {
         assertEquals(true, false, "Implementer bokstavNr og denne testen");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void inneholdt() {
         assertEquals(true, false, "Implementer inneholdt og denne testen");
     }

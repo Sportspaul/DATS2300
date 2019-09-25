0ee9379115a9a8e54b035fd78604514e5998a899
Paul Høglend <pmhoglend@gmail.com>
2019-09-25 11:14:48 +0200

    Gjort testen i oppgave 0, ingen uventede feilmeldinger

---
+++ src/Assignments/Oblig2/Oblig2Test/Oblig2Test.java
@@ -0,0 +1,14 @@
+package Assignments.Oblig2.Oblig2Test;
+
+import Assignments.Oblig2.DobbeltLenketListe;
+import Assignments.Oblig2.Liste;
+import org.junit.jupiter.api.Test;
+
+public class Oblig2Test {
+
+
+    @Test
+    void instansTest() {
+        Liste<String> liste = new DobbeltLenketListe<>();
+    }
+}
